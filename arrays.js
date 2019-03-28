7108ef10fc6daf6b8cf685086adc034d5ef2954c
Dikla Beauliere <dikla.beauliere@gmail.com>
2019-03-28 00:15:34 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,44 @@
+/*var thing = "beans";
+
+var carb =[
+  'bread',
+  'rice',
+  'potato'
+];
+
+var keto=[
+  'peanut',
+  'cheese',
+  'bacon'
+];
+function addElementToBeginningOfArray(array1, item){
+  console.log(array1.unshift(item));
+
+ }
+addElementToBeginningOfArray(carb, thing);*/
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+];
+
+//var item ="bonbonsiwo";
+function addElementToBeginningOfArray(array, element){
+  var array1 =[element, ...array];
+  return array1;
+}
+//addElementToBeginningOfArray(chocolateBars, item);
+//var item ="bonbonsiwo";
+function destructivelyAddElementToBeginningOfArray(array, element){
+var array1 = array.unshift(element);
+return array;
+}
+function addElementToEndOfArray(array, element){
+  var array1 =[...array, element];
+  return array1;
+}
+function destructivelyAddElementToEndOfArray(array, element){
+var array1 = array.push(element);
+return array;
+}
